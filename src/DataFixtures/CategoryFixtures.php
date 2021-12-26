3673e209a20760d59aa1811b8f037b01b2a0a976
khalifa-ch <khalifachelbi06@gmail.com>
2021-12-26 11:58:33 +0100

    first commit

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        designCategory = new Category();
+        $designCategory->setName('Design');
+
+        $programmingCategory = new Category();
+        $programmingCategory->setName('Programming');
+
+        $managerCategory = new Category();
+        $managerCategory->setName('Manager');
+
+        $administratorCategory = new Category();
+        $administratorCategory->setName('Administrator');
+
+        $manager->persist($designCategory);
+        $manager->persist($programmingCategory);
+        $manager->persist($managerCategory);
+        $manager->persist($administratorCategory);
+
+        $manager->flush();
+
+        $this->addReference('category-design', $designCategory);
+        $this->addReference('category-programming', $programmingCategory);
+        $this->addReference('category-manager', $managerCategory);
+        $this->addReference('category-administrator', $administratorCategory);
+    }
+}
